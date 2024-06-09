255b56dae23b8a98199cf639d75cac7140819f9e
EMachad0 <elitonmachadod200@gmail.com>
2024-06-09 17:40:58 -0300

    tomando pro deploy

--- app.config.ts
+++ app.config.ts
@@ -1,8 +1,3 @@
 import { defineConfig } from '@solidjs/start/config';
 
-export default defineConfig({
-  vite: {
-    base: '/devo-ir-pra-udesc-hoje/',
-    ssr: true,
-  },
-});
+export default defineConfig({});
